83f6119a2b333844ecbf35140fc9c67394b932ac
thanhnvph22301 <thanhnvph22301@fpt.edu.vn>
2023-03-13 22:46:06 +0700

    update code 130323

--- app/views/admin/body/slibar.blade.php
+++ app/views/admin/body/slibar.blade.php
@@ -37,7 +37,10 @@
               <a href="{{route('contact-us')}}">Contact Us</a>
             </li>
             <li class="">
-              <a href="{{route('instagram')}}">instagram</a>
+              <a href="{{route('instagram')}}">Instagram</a>
+            </li>
+            <li class="">
+              <a href="{{route('staff-list')}}">Quản lý nhân viên</a>
             </li>
           </ul>
         </li>
