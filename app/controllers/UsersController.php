b3cf400c2fc36d624eda97b923ff66c5b238efcf
thanhnvph22301 <thanhnvph22301@fpt.edu.vn>
2023-03-18 16:11:54 +0700

    update code 180323

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -99,6 +99,10 @@ class UsersController extends BaseController
         $this->render('users.updateusers', compact('showUpdate'));
     }
     public function updateProfile($id){
+
+
+
+
         if(isset($_POST["btn-updateuser"])){
             $errors = [] ;
             if (empty($_POST['username'])) {
