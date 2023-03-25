3355140735f2b62c19c09f8bf4f6ed55373e446d
thanhnvph22301 <thanhnvph22301@fpt.edu.vn>
2023-03-25 17:50:14 +0700

    update contact, endow 250323

--- app/models/Endow.php
+++ app/models/Endow.php
@@ -3,5 +3,10 @@ namespace App\models;
 
 class Endow extends boostrap{
     protected $table = 'endow';
+    public function addEndow($name, $phone, $create){
+        $sql = "INSERT INTO $this->table VALUES (NULL, '$name', '$phone', '$create')";
+        $this->setQuery($sql);
+        return $this->execute();
+    }
 }
 ?>
--- app/models/contact.php
+++ app/models/contact.php
@@ -3,5 +3,11 @@ namespace App\models;
 
 class contact extends boostrap{
     protected $table = 'contact';
+
+    public function addContact($name, $email, $problem, $message, $create){
+        $sql = "INSERT INTO $this->table VALUES (NULL, '$name', '$email', '$problem', '$message', '$create')";
+        $this->setQuery($sql);
+        return $this->execute();
+    }
 }
 ?>
